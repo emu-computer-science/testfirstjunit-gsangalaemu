41b6fd395c0f694342c61e30ab9b03f2838eb8e8
gsangalaemu <gsangala@emich.edu>
2025-03-16 06:51:50 -0400

    Created tests for setDate

---
+++ src/testingDates/DateAddDaysTest.java
@@ -0,0 +1,66 @@
+package testingDates;
+// Step 2 said to use the same name as the package in Date.java,
+// which is "buildToTestLab", however step 3 says to name the
+// package "testingDates". I wasn't sure which I was supposed to
+// use, so I followed step 3
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DateAddDaysTest {
+	
+	// Test adding one day in the same month
+	
+	@Test
+	public void testDaySameMonthJan() {
+		Date sameMonthJan = new Date(1, 3, 2019);
+		Date sameMonthJanExpectedNew = new Date(1, 4, 2019);
+		Date.addOneDay(sameMonthJan);
+		assertTrue(sameMonthJan.equals(sameMonthJanExpectedNew));		
+	};
+	
+	@Test
+	public void testDaySameMonthFeb() {
+		Date sameMonthFeb = new Date(2, 20, 2024);
+		Date sameMonthFebExpectedNew = new Date(2, 21, 2024);
+		Date.addOneDay(sameMonthFeb);
+		assertTrue(sameMonthFeb.equals(sameMonthFebExpectedNew));		
+	};
+	
+	@Test
+	public void testDaySameMonthOct() {
+		Date sameMonthOct = new Date(10, 30, 2025);
+		Date sameMonthOctExpectedNew = new Date(10, 31, 2025);
+		Date.addOneDay(sameMonthOct);
+		assertTrue(sameMonthOct.equals(sameMonthOctExpectedNew));		
+	};
+	
+	// Test adding one day crossing a month boundary
+	
+	@Test
+	public void testDayAcrossMonthAprilMay() {
+		Date acrossAprilMay = new Date(4, 30, 2025);
+		Date acrossAprilMayExpectedNew = new Date(5, 1, 2025);
+		Date.addOneDay(acrossAprilMay);
+		assertTrue(acrossAprilMay.equals(acrossAprilMayExpectedNew));		
+	};
+	
+	@Test
+	public void testDayAcrossMonthOctNov() {
+		Date acrossMonthOctNov = new Date(10, 31, 2025);
+		Date acrossMonthOctNovExpectedNew = new Date(11, 1, 2025);
+		Date.addOneDay(acrossMonthOctNov);
+		assertTrue(acrossMonthOctNov.equals(acrossMonthOctNovExpectedNew));		
+	};
+	
+	// Test adding one day crossing a year boundary
+	
+	@Test
+	public void testDayAcrossYear() {
+		Date acrossMonthYear = new Date(12, 31, 2025);
+		Date acrossMonthYearExpectedNew = new Date(1, 1, 202);
+		Date.addOneDay(acrossMonthYear);
+		assertTrue(acrossMonthYear.equals(acrossMonthYearExpectedNew));		
+	};
+	
+
+}
---
+++ src/testingDates/DateSetDateTest.java
@@ -0,0 +1,68 @@
+package testingDates;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DateSetDateTest {
+
+	// Test for legal values, changes to the Date should occur
+	
+	@Test
+	public void testLegalValueJan() {
+		Date legalValueJan = new Date(1, 3, 2019);
+		Date dateLegalValueTest = new Date();
+		dateLegalValueTest.setDate(1, 3, 2019);
+		assertTrue(legalValueJan.equals(dateLegalValueTest));	
+	}
+	
+	@Test
+	public void testLegalValueMar() {
+		Date legalValueMar = new Date(3, 15, 2024);
+		Date dateLegalValueTest = new Date();
+		dateLegalValueTest.setDate(3, 15, 2024);
+		assertTrue(legalValueMar.equals(dateLegalValueTest));	
+	}
+	
+	@Test
+	public void testLegalValueJun() {
+		Date legalValueJun = new Date(6, 20, 2435);
+		Date dateLegalValueTest = new Date();
+		dateLegalValueTest.setDate(6, 20, 2435);
+		assertTrue(legalValueJun.equals(dateLegalValueTest));	
+	}
+		
+	// Test for illegal values, changes to the Date should not occur
+	
+	@Test
+	public void testIllegalValueJan() {
+		Date illegalValueJan = new Date(1, 32, 2025);
+		Date dateIllegalValueTest = new Date();
+		dateIllegalValueTest.setDate(1, 32, 2025);\
+		// Check to see if the date was changed to our test value
+		// Assert false, as expected behaviour is the date was not
+		// changed and the values are not equal
+		assertFalse(illegalValueJan.equals(dateIllegalValueTest));	
+	}
+	
+	@Test
+	public void testIllegalValueFeb() {
+		Date illegalValueFeb = new Date(2, 30, 2026);
+		Date dateIllegalValueTest = new Date();
+		dateIllegalValueTest.setDate(2, 30, 2026);
+		// Check to see if the date was changed to our test value
+		// Assert false, as expected behaviour is the date was not
+		// changed and the values are not equal
+		assertFalse(illegalValueFeb.equals(dateIllegalValueTest));	
+	}
+	
+	@Test
+	public void testIllegalValueApr() {
+		Date illegalValueApr = new Date(4, 31, 2492);
+		Date dateIllegalValueTest = new Date();
+		dateIllegalValueTest.setDate(4, 31, 2492);
+		// Check to see if the date was changed to our test value
+		// Assert false, as expected behaviour is the date was not
+		// changed and the values are not equal
+		assertFalse(illegalValueApr.equals(dateIllegalValueTest));	
+	}
+	
+}
